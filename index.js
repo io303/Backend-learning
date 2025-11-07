bfd85780c23b4bd06c183f8cec90c7c91f8e779b
io303 <srahul12768@gmail.com>
2025-11-07 14:22:49 +0530

    basic

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+
+
+const express = require('express')  // importing expreess
+const app = express()  
+const port = 3000 // virtual port 
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/twitter',(req,res)=>{
+  res.send('hiteshdotcom')
+})
+app.get('/login',(req,res)=>{
+  res.send('<h1>please login at rahul.com')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.listen(process.env.PORT,()=>{
+  console.log(`Example app listening on port ${port}`);
+  
+})
+
